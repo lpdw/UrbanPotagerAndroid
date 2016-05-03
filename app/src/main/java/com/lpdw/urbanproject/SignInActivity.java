554c679ff19028c882110e188b088a5290ad665f
Yassin AGHANI <aghani.yassin@gmail.com>
2016-05-03 10:15:02 +0200

    Add sign in screen

--- app/src/main/java/com/lpdw/urbanproject/SignInActivity.java
+++ app/src/main/java/com/lpdw/urbanproject/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.lpdw.urbanproject;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        String title = getResources().getString(R.string.SignIn);
+        actionBar.setTitle(title);
     }
 }
