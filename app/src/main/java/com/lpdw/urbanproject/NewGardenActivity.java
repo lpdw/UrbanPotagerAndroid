328e5f796d0b9d661b1d21f386715c8c9dc39b7b
Yassin AGHANI <aghani.yassin@gmail.com>
2016-05-03 10:31:37 +0200

    Add new garden screen

---
+++ app/src/main/java/com/lpdw/urbanproject/NewGardenActivity.java
@@ -0,0 +1,15 @@
+package com.lpdw.urbanproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yassin on 03/05/16.
+ */
+public class NewGardenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_garden);
+    }
+}
--- app/src/main/java/com/lpdw/urbanproject/SignInActivity.java
+++ app/src/main/java/com/lpdw/urbanproject/SignInActivity.java
@@ -1,10 +1,13 @@
 package com.lpdw.urbanproject;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,18 @@ public class SignInActivity extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         String title = getResources().getString(R.string.SignIn);
         actionBar.setTitle(title);
+
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_in_button){
+            Intent intent = new Intent(this, NewGardenActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
 }
